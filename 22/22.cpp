b37bdb16e4929c0a57a8984aed26341d067a40ab
lvjing <704074520@qq.com>
2023-02-07 18:31:46 +0800

    对于dfs和回溯的形式的有些讨论 搭配18

--- 22/22.cpp
+++ 22/22.cpp
@@ -8,11 +8,11 @@ void op(int n,string now,int sul,int sur)
 {
     if(sul<sur||sur>n||sul>n) return;
     if(sul==n&&sur==n)
-     ret.push_back(now);
-     if(sul+sur>2*n) return; 
+     ret.push_back(now); 
      op(n,now+'(',sul+1,sur);
      op(n,now+')',sul,sur+1);
 }
+
 int main(void)
 {
     int n=3;
@@ -23,22 +23,23 @@ int main(void)
         cout<<ret[i]<<"     ";
     }
 }
-//vector<string> generateParenthesis(int n) {
-        
-//         while(ret.size()!=0)
-//         {
-//             now.push_back('(');
-//             sul++;
-//             if(sul==sur&&sul==n/2)
-//             {
-//                 ret.push_back(now);
-//             }
-//             else{
-//                 sul--;
-//                 now.pop_back();
-//             }
-//             if();
+vector<string> generateParenthesis(int n) {
+        string now="(";
+        int sul=1,sur=0;
+        while(now.size()!=0)
+        {
+            now.push_back('(');
+            sul++;
+            if(sul==sur&&sul==n/2)
+            {
+                ret.push_back(now);
+            }
+            else{
+                sul--;
+                now.pop_back();
+            }
+            if();
 
 
-//         }
-// }
+        }
+}
